e0c08a65735079d63bafbd408138b2f1d0dd1a65
glenn <glenn@gpjones.org>
2016-03-17 11:18:19 +0100

    pass right data through to template

--- config.rb
+++ config.rb
@@ -47,10 +47,18 @@ activate :directory_indexes
 
 startups = JSON.load(open("https://nebulae-dashboard.herokuapp.com/startups-index.json"))
 startups.each do |s|
-  proxy "/#{s["name"].downcase.split(" ").join("-")}/index.html", "/startup-template.html", :locals => {  name:         s["name"],
-                                                                                                                            summary:      s["summary"],
-                                                                                                                            founders:     s["founders"],
-                                                                                                                            website_url:  s["website_url"] }, ignore: true
+  proxy "/#{s["name"].downcase.split(" ").join("-")}/index.html", 
+        "/startup-template.html", 
+        :locals => {  name:             s["name"],
+                      summary:          s["summary"],
+                      founders:         s["founders"],
+                      address:          s["address"],
+                      employee_count:   s["employee_count"],
+                      website_url:      s["website_url"],
+                      twitter_url:      s["twitter_url"],
+                      logo_url:         s["logo_url"],
+                      market_domain:    s["market_domain"] }, 
+        ignore: true
 end
 
 
