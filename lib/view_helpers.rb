c0a2f55860d573d496b5740b70ebcb0016a59e9f
nedzen <nedzen.11@gmail.com>
2016-04-26 20:25:01 +0200

    added sharepay logo

--- lib/view_helpers.rb
+++ lib/view_helpers.rb
@@ -1,33 +1,33 @@
 module ViewHelpers
-  def startup_title_helper
-    if current_page.locals[:name]
-      current_page.locals[:name] + " - " + data.site.title
-    else
-      data.site.title
-    end
-  end
+	def startup_title_helper
+		if current_page.locals[:name]
+			current_page.locals[:name] + " - " + data.site.title
+		else
+			data.site.title
+		end
+	end
 
-  def startup_description_helper
-    if  current_page.locals[:name] && 
-        current_page.locals[:market_domain] &&
-        current_page.locals[:founders] &&
-        current_page.locals[:address]
-      name = current_page.locals[:name]
-      market_domain = current_page.locals[:market_domain]
-      founders = current_page.locals[:founders]
-      address = current_page.locals[:address]
+	def startup_description_helper
+		if  current_page.locals[:name] && 
+				current_page.locals[:market_domain] &&
+				current_page.locals[:founders] &&
+				current_page.locals[:address]
+			name = current_page.locals[:name]
+			market_domain = current_page.locals[:market_domain]
+			founders = current_page.locals[:founders]
+			address = current_page.locals[:address]
 
-      return "#{name.capitalize} is a startup that is involved in #{market_domain}. It's founders are #{founders} and they are based at #{address}."
-    else
-      "Is a startup based in Paris. They employ multiple people and are interested in expanding their business."
-    end
-  end
+			return "#{name.capitalize} is a startup that is involved in #{market_domain}. It's founders are #{founders} and they are based at #{address}."
+		else
+			"Is a startup based in Paris. They employ multiple people and are interested in expanding their business."
+		end
+	end
 
-  def startup_image_helper
-    current_page.locals[:logo_url] ||= 'http://gph.is/148YzQp'
-  end
+	def startup_image_helper
+		current_page.locals[:logo_url] ||= 'http://gph.is/148YzQp'
+	end
 
-  def strip_link(incoming_url)
+	def strip_link(incoming_url)
 
 		def strip_www(url)
 			if url.nil?
@@ -64,8 +64,13 @@ module ViewHelpers
 
 		result = twitter_stripped
 		return result
-  end
-
+	end
+	
+	def words_limit(text, limit)
+		string_arr = text.split(' ')
+		string_arr.count > limit ? "#{text[0..(limit-1)].join(' ')}..." : text
+	end
+	
 	def root_url(rel_link)
 		"http://parisianstartups.com/#{rel_link}"
 	end
